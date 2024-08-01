59225af4482a9439c8bdcbae0658c5478a4a226f
전현오 <139675173+JeffJPortfolio@users.noreply.github.com>
2024-08-01 13:08:36 +0900

    styling

--- src/com/javaex/librarian/LibrarianApp.java
+++ src/com/javaex/librarian/LibrarianApp.java
@@ -6,243 +6,204 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
 
-
 public class LibrarianApp {
-	public static void main(String[] args) throws IOException{
-		
-		Scanner sc = new Scanner(System.in);
-		boolean run = true;
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        boolean run = true;
 
-		
-		System.out.println("관리자 페이지에 온걸 환영 합니다");
-		System.out.println("행등을 선택 하십시오");
-		
-		while (run) {
-			BookDao bookDao = new BookDao();
-			UserDao userDao = new UserDao();
-			
-			
-			List<BookVo> BookList = bookDao.selectBookAll(); //new리스트만들어서 주소만 전달한다
-			List<UserVo> UserList = userDao.selectUserAll();
-			
-			// 메뉴 출력
-			System.out.println("");
-			System.out.println("1.대여등록		2.반납등록		3.대여 현황	4.회원관리		5.책관리		6.종료");
-			System.out.println("--------------------------------------------------------------------------");
-			System.out.print(">메뉴번호: ");
+        System.out.println("♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡");
+        System.out.println("♡            관리자 페이지에 오신 것을            ♡");
+        System.out.println("♡                환영합니다!                        ♡");
+        System.out.println("♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡");
+        
+        while (run) {
+            BookDao bookDao = new BookDao();
+            UserDao userDao = new UserDao();
 
-			// 메뉴 입력
-			int menuNum = sc.nextInt();
-			sc.nextLine(); // 숫자 뒤에 문자열 받을때 생기는 오류 수정용
+            List<BookVo> bookList = bookDao.selectBookAll();
+            List<UserVo> userList = userDao.selectUserAll();
 
-			int bookId;
-			int memberId;
-			
-			switch (menuNum) {
-			
-				case 1: //대여등록
-					System.out.print("회원id:");
-					String user_id = sc.nextLine();
-					int userNum = bookDao.idToNum(user_id);
-					//애러 확인
-					if (userNum == -1) {
-						System.out.println("해당 아이디로 등록된 사용자가 없습니다");
-						break;
-					}			
-					System.out.print("책id:");
-					int bid = sc.nextInt();
-					bookDao.insertRent(bid, userNum);
+            // 메뉴 출력
+            System.out.println("");
+            System.out.println("     ▶ 원하시는 메뉴를 선택해주세요 ◀");
+            System.out.println("♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡");
+            System.out.println("♡1. 대여 등록       2. 반납 등록      ♡");
+            System.out.println("♡3. 대여 현황       4. 회원 관리      ♡");
+            System.out.println("♡5. 책 관리        6. 종료          ♡");
+            System.out.println("♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡");
+            System.out.print(" ▶ 메뉴 번호: ");
 
-					System.out.println("책 " +BookList.get(bid).getTitle() + "가 대여 되었습니다");
-					break;
-			
-				case 2: //반납등록
-					System.out.println("<2.반납등록>");
-					System.out.print(">책 번호: ");
-					bookId = sc.nextInt();
-					//애러 확인
-					if(BookList.get(bookId - 1).getB_status() == -1 || BookList.get(bookId - 1).getB_status() == 0) {
-						System.out.println("해당 아이디로 등록된 책이 없습니다");
-						break;
-					}
-					
-					bookDao.updateReturn(bookId); 
-					System.out.println("책 " +BookList.get(bookId).getTitle() + "이 반납 되었습니다");
-					break;	
-				
-				case 3: //대여 현황
-					System.out.println("<3.대여 현황>");
-					for(int i = 0; i < BookList.size(); i++) {
-						if (BookList.get(i).getB_status() != 0) {
-							System.out.println(BookList.get(i).toString());
-						}
-					}
-					break;
-				
-			// 4(회원관리)
-			//MemberList
-				case 4:
-					System.out.println("<4.회원관리>");
-					
-					for(int i = 0; i < UserList.size(); i++) {
-						System.out.println(UserList.get(i).toString());
+            // 메뉴 입력
+            int menuNum = sc.nextInt();
+            sc.nextLine(); // 숫자 뒤에 문자열 받을 때 생기는 오류 수정용
 
-					}
+            int bookId;
+            int memberId;
 
-					System.out.println("회원 아이디 찾기 ");
-					System.out.print(">전화번호: ");
-					String keyword = sc.nextLine();
-					
-					if (keyword.equals("")) {
-						break;
-					}
-					else {
-						showUserList(UserList, keyword) ;//키워드를 포함한 리스트 출력
-					}
-					break;	
-			
-				// 5(책관리)
-				case 5:
-					System.out.println("<5.책관리>");
-					boolean temp = true;
-					while(temp) {
-						temp = bookManagement();
-					}
-					
-					
-					break;					
-			
-			// 6(종료)
-				case 6:
-					run = false;
-					break;
+            switch (menuNum) {
+                case 1: // 대여 등록
+                    System.out.print("▶ 회원 ID를 입력하세요: ");
+                    String userId = sc.nextLine();
+                    int userNum = bookDao.idToNum(userId);
 
-			// 없는 번호일때
-				default:
-					System.out.println("[다시 입력해 주세요.]");
-					break;
-				
-			}// switch() 종료
-		
-		}
-		System.out.println("");
-		System.out.println("******************************************");
-		System.out.println("*                   감사합니다           *");
-		System.out.println("******************************************");
+                    if (userNum == -1) {
+                        System.out.println("※ 해당 ID로 등록된 사용자가 없습니다. ※");
+                        break;
+                    }
 
-	}
-		
-		
-	public static boolean bookManagement() {
-		boolean loop = true;
-		BookDao bookDao = new BookDao();
-		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("행등을 선택 하십시오");
-			// 메뉴 출력
-		System.out.println("");
-		System.out.println("1.책등록		2.책수정		3.책 삭제		4.종료");
-		System.out.println("--------------------------------------------------------------------------");
-		System.out.print(">메뉴번호: ");
-		
-		int menuNum = sc.nextInt();
-		sc.nextLine(); // 숫자 뒤에 문자열 받을때 생기는 오류 수정용
-		
-		switch (menuNum) {
+                    System.out.print("▶ 책 ID를 입력하세요: ");
+                    int bookIdForRent = sc.nextInt();
+                    bookDao.insertRent(bookIdForRent, userNum);
 
-		case 1: //책등록
-			System.out.println("<1.책등록>");
-			System.out.print(">제목: ");
-			String title = sc.nextLine();
-			System.out.print(">작가: ");
-			String author = sc.nextLine();
-			System.out.print(">출판사: ");
-			String pubs = sc.nextLine();
-			System.out.print(">출판일: ");
-			String pubs_date = sc.nextLine();
-			int count = bookDao.insertBook(title, author, pubs, pubs_date);
-			break;
-		
-		case 2: //책수정
-			System.out.println("<2.책수정>");
-			//인스트럭션 쓰기
-			System.out.print(">수정할 책 번호: ");
-			int bookid = sc.nextInt();
-			sc.nextLine();
-			System.out.println(">제목: ");
-			String alterTitle = sc.nextLine();
-			System.out.print(">작가: ");
-			String alterAuthor = sc.nextLine();
-			System.out.print(">출판사: ");
-			String alterPubs = sc.nextLine();
-			System.out.print(">출판일: ");
-			String alterPubs_date = sc.nextLine();
-			//updateBook(bookid, alterTitle, alterAuthor, alterPubs, alterPubs_date);
-			break;	
-			
-		case 3: //삭제
-			System.out.println("<3.책 삭제>");
-			System.out.print(">삭제할 책 번호: ");
-			int delBookid = sc.nextInt();
-			bookDao.delBook(delBookid);
-			break;
+                    System.out.println("♥ 책 \"" + bookList.get(bookIdForRent).getTitle() + "\"가 대여되었습니다. ♥");
+                    break;
 
-		// 5(종료)
-		case 4:
-			loop = false;
-			break;
+                case 2: // 반납 등록
+                    System.out.println("<반납 등록>");
+                    System.out.print("▶ 책 번호를 입력하세요: ");
+                    bookId = sc.nextInt();
 
-		// 없는 번호일때
-		default:
-			System.out.println("[다시 입력해 주세요.]");
-			break;
-			
-		}// switch() 종료
+                    if (bookId >= bookList.size() || bookId < 0) {
+                        System.out.println("※ 유효하지 않은 책 번호입니다. ※");
+                        break;
+                    }
 
-		return loop;
-		
-		
-	}
-		
-		
-		
-		
-/*	public static void showBookList(List<BookVo> bookList) {
-		showBookList(bookList, ""); // 아래함수에 키워드를 아무것도 없는 값으로 전달
-		}
-	public static void showBookList(List<BookVo> bookList, String keyword) {
-		for (int i = 0; i < bookList.size(); i++) {
-			String serchPhone = bookList.get(i).get();
-			if (serchPhone.contains(keyword)) {
-				System.out.print(i + 1 + ".   ");
-				System.out.print(bookList.get(i).getName() + "\t");
-				System.out.print(bookList.get(i).getHp() + "\t");
-				System.out.print(bookList.get(i).getCompany() + "\t");
-				System.out.println("");
-			}
-			else {
-				System.out.println("해당 책이 없습니다")
-			}
-		}*/
-		
-	
-	
-	
-	// 나중에 전화번호로 등록된거 없음 없다라고 표시하게 하기
-	public static void showUserList(List<UserVo> userList) {
-		showUserList(userList, ""); // 아래함수에 키워드를 아무것도 없는 값으로 전달
-		}
-	
-	public static void showUserList(List<UserVo> userList, String keyword) {
-		for (int i = 0; i < userList.size(); i++) {
-			String serchName = userList.get(i).getPhoneNum();
-			if (serchName.contains(keyword)) {
-				System.out.print(userList.get(i).toString());
-			}
-			
-		}
-		
-	}
-	
-	
+                    if (bookList.get(bookId).getB_status() == -1) {
+                        System.out.println("※ 이미 반납된 책입니다. ※");
+                        break;
+                    }
+
+                    bookDao.updateReturn(bookId);
+                    System.out.println("♥ 책 \"" + bookList.get(bookId).getTitle() + "\"가 반납되었습니다. ♥");
+                    break;
+
+                case 3: // 대여 현황
+                    System.out.println("<대여 현황>");
+                    for (BookVo book : bookList) {
+                        if (book.getB_status() != 0) {
+                            System.out.println(book);
+                        }
+                    }
+                    break;
+
+                case 4: // 회원 관리
+                    System.out.println("<회원 관리>");
+                    for (UserVo user : userList) {
+                        System.out.println(user);
+                    }
+
+                    System.out.print("▶ 전화번호로 회원 검색: ");
+                    String phoneKeyword = sc.nextLine();
+
+                    if (!phoneKeyword.isEmpty()) {
+                        showUserList(userList, phoneKeyword);
+                    }
+                    break;
+
+                case 5: // 책 관리
+                    System.out.println("<책 관리>");
+                    boolean temp = true;
+                    while (temp) {
+                        temp = bookManagement();
+                    }
+                    break;
+
+                case 6: // 종료
+                    run = false;
+                    break;
+
+                default:
+                    System.out.println("※ 유효하지 않은 옵션입니다. 다시 시도해주세요. ※");
+                    break;
+            }
+        }
+
+        System.out.println("");
+        System.out.println("♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡");
+        System.out.println("♡        사용해 주셔서 감사합니다!         ♡");
+        System.out.println("♡       좋은 하루 보내세요!              ♡");
+        System.out.println("♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡");
+    }
+
+    public static boolean bookManagement() {
+        boolean loop = true;
+        BookDao bookDao = new BookDao();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("");
+        System.out.println("     ▶ 책 관리 메뉴를 선택하세요 ◀");
+        System.out.println("♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡");
+        System.out.println("♡1. 책 등록      2. 책 수정        ♡");
+        System.out.println("♡3. 책 삭제      4. 메인 메뉴로 돌아가기♡");
+        System.out.println("♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡");
+        System.out.print(" ▶ 메뉴 번호: ");
+
+        int menuNum = sc.nextInt();
+        sc.nextLine(); // 숫자 뒤에 문자열 받을 때 생기는 오류 수정용
+
+        switch (menuNum) {
+            case 1: // 책 등록
+                System.out.println("<책 등록>");
+                System.out.print("▶ 제목: ");
+                String title = sc.nextLine();
+                System.out.print("▶ 작가: ");
+                String author = sc.nextLine();
+                System.out.print("▶ 출판사: ");
+                String publisher = sc.nextLine();
+                System.out.print("▶ 출판일: ");
+                String pubDate = sc.nextLine();
+                bookDao.insertBook(title, author, publisher, pubDate);
+                System.out.println("♥ 책이 등록되었습니다. ♥");
+                break;
+
+            case 2: // 책 수정
+                System.out.println("<책 수정>");
+                System.out.print("▶ 수정할 책 번호를 입력하세요: ");
+                int bookId = sc.nextInt();
+                sc.nextLine();
+                System.out.print("▶ 새 제목: ");
+                String newTitle = sc.nextLine();
+                System.out.print("▶ 새 작가: ");
+                String newAuthor = sc.nextLine();
+                System.out.print("▶ 새 출판사: ");
+                String newPublisher = sc.nextLine();
+                System.out.print("▶ 새 출판일: ");
+                String newPubDate = sc.nextLine();
+                bookDao.updateBook(bookId, newTitle, newAuthor, newPublisher, newPubDate); // Uncomment when method is implemented
+                System.out.println("♥ 책이 수정되었습니다. ♥");
+                break;
+
+            case 3: // 책 삭제
+                System.out.println("<책 삭제>");
+                System.out.print("▶ 삭제할 책 번호를 입력하세요: ");
+                int delBookId = sc.nextInt();
+                bookDao.delBook(delBookId);
+                System.out.println("♥ 책이 삭제되었습니다. ♥");
+                break;
+
+            case 4: // 메인 메뉴로 돌아가기
+                loop = false;
+                break;
+
+            default:
+                System.out.println("※ 유효하지 않은 옵션입니다. 다시 시도해주세요. ※");
+                break;
+        }
+
+        return loop;
+    }
+
+    public static void showUserList(List<UserVo> userList, String keyword) {
+        boolean found = false;
+        for (UserVo user : userList) {
+            if (user.getPhoneNum().contains(keyword)) {
+                System.out.println(user);
+                found = true;
+            }
+        }
+        if (!found) {
+            System.out.println("※ 제공된 전화번호로 등록된 회원이 없습니다. ※");
+        }
+    }
 }
+
